53e1b15601070c002bbef70213078aabfe92f97e
Gabriel Santos Souto <107152342+gabrielsantossouto@users.noreply.github.com>
2025-03-21 16:05:54 -0300

    update

---
+++ sript.js
@@ -0,0 +1,12 @@
+let img = document.getElementById('borda')
+let clicado = true
+
+function clicou(){
+    if (clicado == true){
+        clicado = false
+        img.style.border = '2px solid #ffff'
+    }else {
+        img.style.border = ''
+        clicado = true
+    }
+}
